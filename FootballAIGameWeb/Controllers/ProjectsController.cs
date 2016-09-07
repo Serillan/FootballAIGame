19a2f64ce3b8564dd7e963a09280730b0b12abf0
Serillan <serillan11@gmail.com>
2016-09-07 22:07:55 +0200

    Add CSharp Project View.

---
+++ FootballAIGameWeb/Controllers/ProjectsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FootballAIGameWeb.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        /// <summary>
+        /// Returns C# AI Project view.
+        /// </summary>
+        /// <returns>The C# AI Project view.</returns>
+        public ActionResult CSharp()
+        {
+            return View();
+        }
+    }
+}
